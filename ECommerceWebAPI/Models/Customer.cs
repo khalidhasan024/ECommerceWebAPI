5e8af6877f2f14733b8e35be74ff44d009f3f146
khalid <khalidhasan024@outlook.com>
2019-12-14 09:26:25 +0600

    Created Customer mode

---
+++ ECommerceWebAPI/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerceWebAPI.Models
+{
+    public class Customer
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public int Age { get; set; }
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+
+    }
+}
