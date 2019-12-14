2abd70dd11b00c747c4869b0c737742097eec762
khalid <khalidhasan024@outlook.com>
2019-12-14 21:44:28 +0600

    Added_migration--Create_customers_table

---
+++ ECommerceWebAPI/Migrations/20191214145047_Create_database_and_customers_table.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ECommerceWebAPI.Migrations
+{
+    public partial class Create_database_and_customers_table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "customers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    FirstName = table.Column<string>(nullable: false),
+                    LastName = table.Column<string>(nullable: false),
+                    Age = table.Column<int>(nullable: false),
+                    Email = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_customers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "customers");
+        }
+    }
+}
